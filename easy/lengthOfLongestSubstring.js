ad89e3222132a9cacdd6f16d1d0d3b576b475ab6
Akinyemi Olumide Emmanuel <akinolu52@gmail.com>
2022-05-03 01:57:56 +0100

    lengthOfLongestSubstring

---
+++ easy/lengthOfLongestSubstring.js
@@ -0,0 +1,34 @@
+/**
+ * A solution to the count the longest substring with k unique characters.
+ * URL: https://leetcode.com/problems/longest-substring-without-repeating-characters/
+ * /
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+    const result = new Map();
+    let count = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        const current = s.charAt(i);
+        if (result.has(current)) {
+            i = result.get(current);
+            result.clear();
+        } else {
+            result.set(current, i);
+        }
+
+        if (result.size > count) {
+            count = result.size;
+        }
+    }
+
+    return count;
+};
+
+console.log(lengthOfLongestSubstring("abcabcbb"));
+console.log(lengthOfLongestSubstring("dvdf"));
+console.log(lengthOfLongestSubstring("bbbbb"));
+console.log(lengthOfLongestSubstring("pwwkew"));
