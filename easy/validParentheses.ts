4bfb49b1feb2f1e6b39be8d40ac3aea5bdf02b84
Akinyemi Olumide Emmanuel <akinolu52@gmail.com>
2022-07-24 00:53:13 +0100

    valid parenthenses

---
+++ easy/validParentheses.ts
@@ -0,0 +1,42 @@
+/**
+ * @url - hhttps://leetcode.com/problems/valid-parentheses/
+ * 
+ * Given a string s containing just the characters 
+ * '(', ')', '{', '}', '[' and ']', 
+ * determine if the input string is valid.
+ * 
+ * @constraint 
+ * 1. Open brackets must be closed by the same type of brackets.
+ * 2. Open brackets must be closed in the correct order.
+ * 
+ * @param string: 
+ * @returns a boolean 
+ */
+
+function isValid(s: string): boolean {
+    s = s.trim().replace(/\s+/g, '');
+    if (s.length % 2 !== 0) return false;
+    if (!((s.charAt(0) !== '(') || (s.charAt(0) !== '{') || (s.charAt(0) !== '['))) return false;
+
+    const maps: Record<string, string> = {
+        ')': '(', 
+        '}': '{', 
+        ']' : '[',
+    };
+
+    const stack: string[] = [];
+
+    for(let i = 0; i < s.length; i++){
+        const char = s[i] as string;
+        if(!char || char === ' ') continue;
+        
+        if (!maps[char]){
+            stack.push(char);
+            continue;
+        }
+        if(stack.pop() !== maps[char]) return false;
+    }
+
+
+    return stack.length === 0;
+};
