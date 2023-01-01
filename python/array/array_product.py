990129242dc7cdcc9162b94e6b9a03e24fa4b175
Akinyemi Olumide Emmanuel <akinolu52@gmail.com>
2023-01-02 00:59:12 +0100

    array solutions

---
+++ python/array/array_product.py
@@ -0,0 +1,60 @@
+
+'''
+Given an integer array nums,
+return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+'''
+
+
+def productExceptSelf(nums):
+
+    result = []
+
+    if 0 not in nums:
+        product = 1
+
+        for num in nums:
+            product *= num
+
+        for num in nums:
+            result.append(int(product / num))
+
+    else:
+        zero_index = []
+        product = 1
+
+        for index, num in enumerate(nums):
+            if num == 0:
+                zero_index.append(index)
+                continue
+
+            product *= num
+        
+        if len(zero_index) > 1:
+            return [0] * len(nums)
+
+        for index, num in enumerate(nums):
+            if index in zero_index:
+                result.append(product)
+                continue
+            
+            result.append(0)
+
+    return result
+
+
+nums = [1, 2, 3, 4]
+
+print(productExceptSelf(nums))
+
+
+nums = [-1, 1, 0, -3, 3]
+
+print(productExceptSelf(nums))
+
+nums = [-1, 1, 0, 0, 3]
+
+print(productExceptSelf(nums))
+
+nums = [0, 0]
+
+print(productExceptSelf(nums))
