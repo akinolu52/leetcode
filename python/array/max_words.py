4617ab9edd2b67b0f2a1b34c11e668941af823ed
Akinyemi Olumide Emmanuel <akinolu52@gmail.com>
2023-01-09 11:04:05 +0100

    max words example

--- python/array/max_words.py
+++ python/array/max_words.py
@@ -14,3 +14,10 @@ def mostWordsFound(sentences):
 sentences = ["alice and bob love leetcode",
              "i think so too", "this is great thanks very much"]
 print(mostWordsFound(sentences))  # 6
+
+sentences = ["please wait", "continue to fight", "continue to win"]
+print(mostWordsFound(sentences))
+
+sentences = ["w jrpihe zsyqn l dxchifbxlasaehj", "nmmfrwyl jscqyxk a xfibiooix xolyqfdspkliyejsnksfewbjom", "xnleojowaxwpyogyrayfgyuzhgtdzrsyococuqexggigtberizdzlyrdsfvryiynhg", "krpwiazoulcixkkeyogizvicdkbrsiiuhizhkxdpssynfzuigvcbovm",
+             "rgmz rgztiup wqnvbucfqcyjivvoeedyxvjsmtqwpqpxmzdupfyfeewxegrlbjtsjkusyektigr", "o lgsbechr lqcgfiat pkqdutzrq iveyv iqzgvyddyoqqmqerbmkxlbtmdtkinlk", "hrvh efqvjilibdqxjlpmanmogiossjyxepotezo", "qstd zui nbbohtuk", "qsdrerdzjvhxjqchvuewevyzlkyydpeeblpc"]
+print(mostWordsFound(sentences))
