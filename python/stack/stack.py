8458aa53fc0103126da1c4cf2f2828db73268ad4
Akinyemi Olumide Emmanuel <akinolu52@gmail.com>
2023-01-18 11:45:40 +0100

    pure stack implementation

--- python/stack/stack.py
+++ python/stack/stack.py
@@ -20,26 +20,6 @@ class Stack:
         return True
 
     def pop(self):
+        if self.isEmpty():
+            return None
         return self.stack.pop()
-
-
-stack = Stack()
-
-print('stack size: ', stack.getSize())
-
-print('stack push 1: ', stack.push(1))
-print('stack push 2: ', stack.push(2))
-print('stack push 3: ', stack.push(3))
-print('stack push 4: ', stack.push(4))
-
-print('stack contains: ', stack)
-
-print('stack size: ', stack.getSize())
-
-print('stack pop: ', stack.pop())
-print('stack pop: ', stack.pop())
-
-print('stack contains: ', stack)
-
-
-print('stack size: ', stack.getSize())
