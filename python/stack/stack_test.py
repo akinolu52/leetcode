a0a8470ef25c3c1013857ebd6fe70a7dd9f9a25e
Akinyemi Olumide Emmanuel <akinolu52@gmail.com>
2023-01-18 11:45:45 +0100

    stack test

---
+++ python/stack/stack_test.py
@@ -0,0 +1,23 @@
+from stack import Stack
+
+stack = Stack()
+
+print('stack size: ', stack.getSize())
+print('stack pop: ', stack.pop())
+
+print('stack push 1: ', stack.push(1))
+print('stack push 2: ', stack.push(2))
+print('stack push 3: ', stack.push(3))
+print('stack push 4: ', stack.push(4))
+
+print('stack contains: ', stack)
+
+print('stack size: ', stack.getSize())
+
+print('stack pop: ', stack.pop())
+print('stack pop: ', stack.pop())
+
+print('stack contains: ', stack)
+
+
+print('stack size: ', stack.getSize())
