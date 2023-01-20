08d983dcdd4914af2f6ffcc33277aecb09e4a001
Akinyemi Olumide Emmanuel <akinolu52@gmail.com>
2023-01-20 03:21:51 +0100

    rework

--- python/stack/postfix_expression.py
+++ python/stack/postfix_expression.py
@@ -1,39 +1,41 @@
 from stack import Stack
 
 
-def postFixEvaluation(s: str) -> int:
+def postFixEvaluation(s) -> int:
+    # def postFixEvaluation(s: str) -> int:
     stack = Stack()
 
     operators = ['-', '+', '*', '/']
 
     for ch in s:
-        if ch.isdigit():
-            stack.push(ch)
-        else:
-            if ch in operators:
-                val1 = stack.pop()
-                val2 = stack.pop()
+        if ch in operators:
+            val1 = stack.pop()
+            val2 = stack.pop()
 
-                if val1 == None or val2 == None:
-                    return -1
+            if val1 == None or val2 == None:
+                return -1
+            else:
+                result = None
+
+                if ch == '-':
+                    result = val2 - val1
+                elif ch == '+':
+                    result = val2 + val1
+                elif ch == '*':
+                    result = val2 * val1
                 else:
-                    result = None
+                    result = val2 / val1
 
-                    val1 = int(val1)
-                    val2 = int(val2)
+                stack.push(int(result))
 
-                    if ch == '-':
-                        result = val2 - val1
-                    elif ch == '+':
-                        result = val2 + val1
-                    elif ch == '*':
-                        result = val2 * val1
-                    else:
-                        result = val2 / val1
-
-                    stack.push(result)
+        else:
+            stack.push(int(ch))
 
     return stack.pop()
 
 
-print(postFixEvaluation("235*+8-"))
+# print(postFixEvaluation("235*+8-"))
+
+print(postFixEvaluation(["10", "6", "9", "3", "+",
+      "-11", "*", "/", "*", "17", "+", "5", "+"]))
+# print(postFixEvaluation("10693+-11*/*17+5+"))
