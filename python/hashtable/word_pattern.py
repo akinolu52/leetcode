aa1985ba986c9ee926324b2a0309e4cca8641744
Akinyemi Olumide Emmanuel <akinolu52@gmail.com>
2023-01-25 13:08:53 +0100

    completed word pattern

--- python/hashtable/word_pattern.py
+++ python/hashtable/word_pattern.py
@@ -1,5 +1,4 @@
 
-# TODO:
 '''
 Given a pattern and a string s, find if s follows the same pattern.
 
@@ -17,44 +16,22 @@ def wordPattern(pattern: str, s: str) -> bool:
     if len(s_arr) != pattern_len:
         return False
 
-    index = 0
-    for word in s_arr:
+    for index, word in enumerate(s_arr):
         first_char = word[0]
         key = pattern[index]
 
-        # print('first_char = {}, key = {}'.format(first_char, key))
-
-        # if key not in lookup and first_char not in lookup.values():
         if key not in lookup:
-            lookup[key] = first_char
-            # pattern_dict[first_char] = key
-        index += 1
-
-    pattern_keys = list(lookup.keys())
-    pattern_values = list(lookup.values())
-
-    print('lookup = {}, pattern_keys = {}, pattern_values = {}'.format(lookup, pattern_keys, pattern_values))
-    # print('lookup = {}, pattern_dict = {}'.format(lookup, pattern_dict))
-
-    # for index, word in enumerate(s_arr):
-    #     first_char = word[0]
-
-    #     if first_char not in pattern_dict:
-    #         return False
-    #     else:
-    #         val = pattern_dict[first_char]
-    #         pattern_value = pattern[index]
-
-    #         if pattern_value != val:
-    #             return False
-    # # print('key = {}, value = {}, p_value = {}'.format(
-    #     first_char, val, pattern_value))
-
+            lookup[key] = word
+            pattern_dict[word] = key
 
     for index, word in enumerate(s_arr):
-        first_char = word[0]
-        print(index, first_char)
+        if word not in pattern_dict:
+            return False
 
+        val = pattern_dict[word]
+        pattern_value = pattern[index]
+        if pattern_value != val:
+            return False
     return True
 
 
