e0104b9dc4b07f6256f8e691a900dca17c46baa2
Akinyemi Olumide Emmanuel <akinolu52@gmail.com>
2023-01-29 01:38:08 +0100

    mostFrequentNumber

---
+++ python/stack/k_occurence.py
@@ -0,0 +1,48 @@
+
+'''
+Input: arr[] = {3, 1, 4, 4, 5, 2, 6, 1}, K = 2
+Output: 4 1
+Explanation:
+Frequency of 4 = 2, Frequency of 1 = 2
+These two have the maximum frequency and 4 is larger than 1.
+
+Input: arr[] = {7, 10, 11, 5, 2, 5, 5, 7, 11, 8, 9}, K = 4
+Output: 5 11 7 10
+Explanation: 
+Frequency of 5 = 3, Frequency of 11 = 2, Frequency of 7 = 2, Frequency of 10 = 1
+These four have the maximum frequency and 5 is largest among rest.
+'''
+
+
+def mostFrequentNumber(arr, k: int):
+    result = []
+    occurrence = {}
+
+    freq = [[] for i in range(len(arr) + 1)]
+
+    print('freq = ', freq)
+
+    for val in arr:
+        if val in occurrence:
+            occurrence[val] += 1
+        else:
+            occurrence[val] = 1
+
+    for n, c in occurrence.items():
+        freq.append(n)
+
+    for i in range(len(freq)-1, 0, -1):
+        for n in freq[i]:
+            result.append(n)
+            if len(result) == K:
+                return result[-1::-1]
+    print('freq = ', freq)
+
+
+    print(occurrence)
+    # print(arr, k)
+
+
+arr = [3, 1, 4, 4, 5, 2, 6, 1]
+K = 2
+print(mostFrequentNumber(arr, K))
