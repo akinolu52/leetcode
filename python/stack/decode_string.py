dfa9a03537a1a1421e4b969bfcddec76ca9cdd60
Akinyemi Olumide Emmanuel <akinolu52@gmail.com>
2023-02-05 00:46:34 +0100

    decode string

--- python/stack/decode_string.py
+++ python/stack/decode_string.py
@@ -12,22 +12,29 @@ def decodeString(s: str) -> str:
 
             while stack:
                 curr = stack.pop()
+            
+                if numberSeen:
+                    if curr == '[':
+                        break
+                    elif curr.isalpha():
+                        stack.append(curr)
+                        break
+
                 if curr.isdigit():
                     numberSeen = True
                     intStack.append(curr)
                 elif curr != '[' and numberSeen == False:
                     charStack.append(curr)
 
-                if numberSeen:
-                    break
+            intVal = int(''.join(intStack)[::-1] or '1')
+            calc = ''.join(charStack)[::-1] * intVal
 
-            calc = ''.join(charStack)[::-1] * int(''.join(intStack)[::-1])
             stack.extend(list(calc))
 
     return ''.join(stack)
 
-
-print(decodeString("3[a]2[bc]"))  # aaabcbc
+# print(decodeString("3[z]2[2[y]pq4[2[jk]e1[f]]]ef")) # zzzyypqjkjkefjkjkefjkjkefjkjkefyypqjkjkefjkjkefjkjkefjkjkefef
+# print(decodeString("3[a]2[bc]"))  # aaabcbc
 print(decodeString("3[a2[c]]"))  # "accaccacc"
-print(decodeString("2[abc]3[cd]ef"))  # abcabccdcdcdef
+# print(decodeString("2[abc]3[cd]ef"))  # abcabccdcdcdef
 # print(decodeString("100[leetcode]"))  #
