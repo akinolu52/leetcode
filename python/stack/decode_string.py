1316d940e46b8e6343189b7b9580f272a6d4267c
Akinyemi Olumide Emmanuel <akinolu52@gmail.com>
2023-02-05 00:09:55 +0100

    decode string

--- python/stack/decode_string.py
+++ python/stack/decode_string.py
@@ -1,33 +1,33 @@
 
 def decodeString(s: str) -> str:
     stack = []
-    intStack = []
-    sIndex = []
 
-    for idx, ch in enumerate(s):
-        if ch == ']':
-            # loop forward till you get the last number
-            numIdxStart = i = intStack.pop()
-            print('numIdxStart ', numIdxStart)
-            multiplierArr = []
-
-            # while i < idx:
-            #     multiplierArr.append(s[i])
-            #     i += 1
-
-            # multiplier = ''.join(map(str,multiplierArr))
-
-            # print('stack -> ', stack, multiplier)
-
-        else:
-            if ch.isdigit():
-                intStack.append(ch)
+    for ch in s:
+        if ch != ']':
             stack.append(ch)
+        else:
+            charStack = []
+            intStack = []
+            numberSeen = False
+
+            while stack:
+                curr = stack.pop()
+                if curr.isdigit():
+                    numberSeen = True
+                    intStack.append(curr)
+                elif curr != '[' and numberSeen == False:
+                    charStack.append(curr)
+
+                if numberSeen:
+                    break
+
+            calc = ''.join(charStack)[::-1] * int(''.join(intStack)[::-1])
+            stack.extend(list(calc))
 
     return ''.join(stack)
 
 
-# print(decodeString("3[a]2[bc]"))
-# print(decodeString("3[a2[c]]"))  # "accaccacc"
-# print(decodeString("2[abc]3[cd]ef"))  # abcabccdcdcdef
-print(decodeString("100[leetcode]"))  # abcabccdcdcdef
+print(decodeString("3[a]2[bc]"))  # aaabcbc
+print(decodeString("3[a2[c]]"))  # "accaccacc"
+print(decodeString("2[abc]3[cd]ef"))  # abcabccdcdcdef
+# print(decodeString("100[leetcode]"))  #
