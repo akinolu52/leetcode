1d9bb200fd76e450b502fb2415d6b4caa5db9469
Akinyemi Olumide Emmanuel <akinolu52@gmail.com>
2023-02-10 14:23:33 +0100

    isMonotonic checker

---
+++ python/stack/monotone.py
@@ -0,0 +1,25 @@
+from itertools import pairwise
+
+
+def isMonotonic(nums) -> bool:
+    isIncreasing = True
+    isDecreasing = True
+
+    for pair in pairwise(nums):
+
+        if pair[1] > pair[0]:
+            isDecreasing = False
+        if pair[1] < pair[0]:
+            isIncreasing = False
+
+    return isDecreasing or isIncreasing
+
+
+nums = [1, 2, 2, 3]
+print(isMonotonic(nums))
+
+nums = [6, 5, 4, 3]
+print(isMonotonic(nums))
+
+nums = [1, 5, 2]
+print(isMonotonic(nums))
