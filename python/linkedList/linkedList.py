049f9d9b643a4589ed801873cf38ea5b184cf835
Akinyemi Olumide Emmanuel <akinolu52@gmail.com>
2023-02-18 13:03:29 +0100

    linkedlist class in python

---
+++ python/linkedList/linkedList.py
@@ -0,0 +1,16 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def printList(self):
+        temp = self.head
+
+        while temp:
+            print(temp.data)
+            temp = temp.next
