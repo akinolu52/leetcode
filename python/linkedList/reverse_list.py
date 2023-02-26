e2ae4d1042ed99f5267626a5b42f1adfb0372c90
Akinyemi Olumide Emmanuel <akinolu52@gmail.com>
2023-02-26 21:55:35 +0100

    reverse list in dev

---
+++ python/linkedList/reverse_list.py
@@ -0,0 +1,69 @@
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def push(self, new_data):
+        node = Node(new_data)
+        node.next = self.head
+        self.head = node
+
+    def reverse(self) -> Node:
+        current = self.head
+        stack = [None]
+
+        while current:
+            stack.append(current.data)
+            current = current.next
+
+        # stack.append(None)
+        print('Stack : ', stack,)
+        
+        self.head  = stack.pop()
+
+        while stack:
+            current.next = stack.pop()
+            current = current.next
+            print('Stack : ', stack)
+
+        current.next = None
+        # print(stack, self.head.next.data)
+
+    def printList(self):
+        current = self.head
+
+        while current:
+            print(current.data, end=' -> ')
+            current = current.next
+        print("\n")
+
+
+ll = LinkedList()
+ll2 = LinkedList()
+
+s = [8, 7, 6, 5, 4, 3, 2, 1]
+for item in s:
+    ll.push(item)
+
+print('linked list before reversal')
+ll.printList()
+ll.reverse()
+print('linked list after reversal')
+ll.printList()
+
+# s = [5, 4, 3, 2, 1]
+# for item in s:
+#     ll2.push(item)
+
+
+# print('linked list before reversal')
+# ll2.printList()
+# ll2.reverse()
+# print('linked list after reversal')
+# ll2.printList()
